cb28010058979ac88c9699d35bbf8779bd7976e7
khaiminh0401 <85891822+khaiminh0401@users.noreply.github.com>
2023-06-29 02:09:24 +0700

    setup router

---
+++ src/Router.tsx
@@ -0,0 +1,15 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "./page/Home";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Home/>,
+      children:[
+        {
+          path: "about",
+          element: <div>About</div>,
+        }
+      ]
+    }
+]);
