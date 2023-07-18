224a7a748063eeea826236639a8726e26689e585
Khang Ho <hohoangkhang18@gmail.com>
2023-07-19 00:47:56 +0700

    updating cart

--- src/components/Layout/UserLayout.tsx
+++ src/components/Layout/UserLayout.tsx
@@ -1,14 +1,15 @@
 import React from "react";
 import Navbar from "../Navbar";
 import Footer from "../Footer";
-import Card from "../Card";
+import Cart from "../Cart";
 
 function UserLayout({ children }: any) {
     return (
         <div className="container-fluid" style={{height:"100vh"}}>
             <Navbar />
             {children}
-            <Card id={1} name="Tee Lab" price={500.01} img="https://bizweb.dktcdn.net/100/415/697/products/1-af0c84b6-d733-4bef-8dcf-d8a7d6bc30b8.jpg?v=1656673697867"/>
+            {/* <Card id={1} name="Tee Lab" price={500.01} img="https://bizweb.dktcdn.net/100/415/697/products/1-af0c84b6-d733-4bef-8dcf-d8a7d6bc30b8.jpg?v=1656673697867"/> */}
+            <Cart></Cart>
             <Footer/>
         </div>
     );
